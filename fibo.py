b2af5d7baa9461b217c4893735e65520c27e33c0
LOLRAPOSO <matheuslimaramalho888@gmail.com>
2026-09-17 09:12:37 -0300

    Correções

--- fibo.py
+++ fibo.py
@@ -1,73 +1,46 @@
 class MiniCPU:
     def __init__(self):
-        self.mem = [0] * 256          # memória
-        self.reg = [0, 0, 0, 0]       # R0-R3
-        self.pc = 0                   # Program Counter
-        self.zf = 0                   # Zero Flag
+        self.mem = [0] * 256          
+        self.reg = [0, 0, 0, 0]       
+        self.pc = 0                   
+        self.zf = 0                   
         self.running = True
         self.ciclo = 0
 
     def load_program(self):
-        # Salta para o início do código em 0x40
-        self.mem[0x00] = 0x07; self.mem[0x01] = 0x40; self.mem[0x02] = 0x00  # JMP 0x40
+        
+        self.mem[0x00] = 0x07; self.mem[0x01] = 0x40; self.mem[0x02] = 0x00  
 
-        # Dados
-        self.mem[0x08] = 0    # zero
-        self.mem[0x09] = 1    # um
-        self.mem[0x0A] = 7    # N = 7
+        
+        self.mem[0x08] = 0    
+        self.mem[0x09] = 1    
+        self.mem[0x0A] = 7    
 
-        # Código a partir de 0x40
-        # MOV R0, 0        ; a = 0
         self.mem[0x40] = 0x05; self.mem[0x41] = 0x00; self.mem[0x42] = 0x00
-        # MOV R1, 1        ; b = 1
         self.mem[0x43] = 0x05; self.mem[0x44] = 0x01; self.mem[0x45] = 0x01
-        # LOAD R2, 0x0A    ; R2 = N = 7
         self.mem[0x46] = 0x01; self.mem[0x47] = 0x02; self.mem[0x48] = 0x0A
-        # LOAD R3, 0x09    ; R3 = 1
         self.mem[0x49] = 0x01; self.mem[0x4A] = 0x03; self.mem[0x4B] = 0x09
-        # SUB R2, R3       ; R2 = N - 1 = 6 (contador)
         self.mem[0x4C] = 0x04; self.mem[0x4D] = 0x02; self.mem[0x4E] = 0x03
 
-        # loop: (0x4F)
-        # LOAD R3, 0x08    ; R3 = 0
         self.mem[0x4F] = 0x01; self.mem[0x50] = 0x03; self.mem[0x51] = 0x08
-        # CMP R2, R3       ; compara contador com 0
         self.mem[0x52] = 0x06; self.mem[0x53] = 0x02; self.mem[0x54] = 0x03
-        # JZ done (0x76)
         self.mem[0x55] = 0x08; self.mem[0x56] = 0x76; self.mem[0x57] = 0x00
 
-        # temp = a + b
-        # MOV R3, 0
         self.mem[0x58] = 0x05; self.mem[0x59] = 0x03; self.mem[0x5A] = 0x00
-        # ADD R3, R0
         self.mem[0x5B] = 0x03; self.mem[0x5C] = 0x03; self.mem[0x5D] = 0x00
-        # ADD R3, R1
         self.mem[0x5E] = 0x03; self.mem[0x5F] = 0x03; self.mem[0x60] = 0x01
 
-        # a = b
-        # MOV R0, 0
         self.mem[0x61] = 0x05; self.mem[0x62] = 0x00; self.mem[0x63] = 0x00
-        # ADD R0, R1
         self.mem[0x64] = 0x03; self.mem[0x65] = 0x00; self.mem[0x66] = 0x01
 
-        # b = temp
-        # MOV R1, 0
         self.mem[0x67] = 0x05; self.mem[0x68] = 0x01; self.mem[0x69] = 0x00
-        # ADD R1, R3
         self.mem[0x6A] = 0x03; self.mem[0x6B] = 0x01; self.mem[0x6C] = 0x03
 
-        # contador--
-        # LOAD R3, 0x09
         self.mem[0x6D] = 0x01; self.mem[0x6E] = 0x03; self.mem[0x6F] = 0x09
-        # SUB R2, R3
         self.mem[0x70] = 0x04; self.mem[0x71] = 0x02; self.mem[0x72] = 0x03
-        # JMP loop (0x4F)
         self.mem[0x73] = 0x07; self.mem[0x74] = 0x4F; self.mem[0x75] = 0x00
 
-        # done: (0x76)
-        # STORE R1, 0x20   ; mem[0x20] = resultado
         self.mem[0x76] = 0x02; self.mem[0x77] = 0x01; self.mem[0x78] = 0x20
-        # HALT
         self.mem[0x79] = 0x0A; self.mem[0x7A] = 0x00; self.mem[0x7B] = 0x00
 
     def fetch(self):
@@ -78,27 +51,27 @@ class MiniCPU:
         return op, a, b
 
     def decode_execute(self, op, a, b):
-        if op == 0x01:   # LOAD
+        if op == 0x01:   
             self.reg[a] = self.mem[b]
-        elif op == 0x02: # STORE
+        elif op == 0x02: 
             self.mem[b] = self.reg[a]
-        elif op == 0x03: # ADD
+        elif op == 0x03: 
             self.reg[a] = (self.reg[a] + self.reg[b]) & 0xFF
-        elif op == 0x04: # SUB
+        elif op == 0x04: 
             self.reg[a] = (self.reg[a] - self.reg[b]) & 0xFF
-        elif op == 0x05: # MOV
+        elif op == 0x05: 
             self.reg[a] = b
-        elif op == 0x06: # CMP
+        elif op == 0x06: 
             self.zf = 1 if self.reg[a] == self.reg[b] else 0
-        elif op == 0x07: # JMP
+        elif op == 0x07: 
             self.pc = a
-        elif op == 0x08: # JZ
+        elif op == 0x08: 
             if self.zf:
                 self.pc = a
-        elif op == 0x09: # JNZ
+        elif op == 0x09: 
             if not self.zf:
                 self.pc = a
-        elif op == 0x0A: # HALT
+        elif op == 0x0A: 
             self.running = False
 
     def trace(self, op, a, b):
